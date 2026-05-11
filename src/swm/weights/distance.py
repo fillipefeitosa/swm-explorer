75684d5a16f375d1517a5903766e6aad5319ba67
fillipefeitosa <fillfeitosa@gmail.com>
2026-05-11 10:53:23 +0200

    Small fixes

--- src/swm/weights/distance.py
+++ src/swm/weights/distance.py
@@ -4,18 +4,23 @@ import libpysal
 
 logger = logging.getLogger(__name__)
 
+
 def create_distance_swm(geodf, threshold=1000, inverse=False):
-    
-    logger.info(f"--- Creating DistanceBand Matrix (Threshold: {threshold}m) ----")    
+
+    logger.info(f"--- Creating DistanceBand Matrix (Threshold: {threshold}m) ----")
     alpha = 1.0
-    if inverse==True: alpha = -1.0
-    distance_swm = DistanceBand.from_dataframe(geodf, threshold=threshold, alpha=alpha, use_index=True)
-    
+    if inverse is True:
+        alpha = -1.0
+    distance_swm = DistanceBand.from_dataframe(
+        geodf, threshold=threshold, alpha=alpha, use_index=True
+    )
+
     return distance_swm
 
+
 def create_knn_swm(geodf, k=4):
-    
+
     logger.info("--- Creating KNN Matrix ----")
     knn_swm = KNN.from_dataframe(geodf, k=k, use_index=True)
-    
+
     return knn_swm
